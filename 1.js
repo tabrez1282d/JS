ebed5a10322adc2f223aaba3617ff8dac6a02ace
tabrez1282d <tabrezdhoriwala@gmail.com>
2025-12-06 15:17:32 +0000

    learn

---
+++ 1.js
@@ -0,0 +1,14 @@
+//conversion string to number
+let value = "code"
+console.log(typeof value);
+
+let valueInNumber = Number(value)
+console.log(typeof valueInNumber);
+
+// Conversion string to boolean
+let loggedIn = "enter"
+console.log(typeof loggedIn);
+
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn)
